1d70f5b4fbe2cc491ae6ee7b1776f12ddcc04cb9
Bogdan <repozitorro@gmail.com>
2021-11-03 15:49:57 +0200

    Fix: heroku

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const compression = require('compression');
+
 const app = express();
-app.use(express.static(__dirname + '/dist/medium-clone'));
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+
-    '/dist/medium-clone/index.html'));});
-app.listen(process.env.PORT || 8080);
+
+app.use(compression());
+
+app.use(express.static(path.join(__dirname, 'dist/mediumclone-angular')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/mediumclone-angular/index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log("running"));
